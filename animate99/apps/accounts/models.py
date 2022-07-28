76357cd5cadc7bc9660427a9c67286027abdbc5f
adeshinafuad <adeshinafuad@gmail.com>
2022-07-28 10:55:33 +0100

    django project setup

---
+++ animate99/apps/accounts/models.py
@@ -0,0 +1,22 @@
+
+from django.db import models
+from uuid import uuid4
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from .managers import CustomUserManager
+
+class UserAccount(AbstractUser):
+    username=None
+    email = models.EmailField(max_length=50, db_index=True, unique=True)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    
+    objects = CustomUserManager()
+    
+    class Meta:
+        ordering = ["email"]
+        verbose_name = "User"
+        
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
