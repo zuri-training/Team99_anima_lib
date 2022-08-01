3ad07774ebd3cc74452b24131960e96c654bea6c
gneisscode <103408439+gneisscode@users.noreply.github.com>
2022-08-01 11:20:39 +0200

    added documentation template, urls, views and static files

--- animate99/apps/main/urls.py
+++ animate99/apps/main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import home
+from .views import documentation
 
 urlpatterns = [
-     path("", home, name='download' )
+     path("", home, name='home' ),
+     path("documentation", documentation, name='documentation' ),
+
 ]
--- animate99/apps/main/views.py
+++ animate99/apps/main/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home (request):
     return render(request, 'main/home.html',)
+
+def documentation (request):
+    return render(request, 'main/documentation.html',)
