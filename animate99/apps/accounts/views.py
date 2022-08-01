dcccc26485b480eb60e61f0e68288f7350b2268b
adeshinafuad <adeshinafuad@gmail.com>
2022-08-01 10:36:17 +0100

    completing user auth

--- animate99/apps/accounts/views.py
+++ animate99/apps/accounts/views.py
@@ -1,48 +1,55 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.http.response import HttpResponse
 from .forms import CreateUserForm
 from django.contrib import messages
-from django.contrib.auth import authenticate,login, logout
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def register_view(request):
-    form = CreateUserForm()
-    
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('email')
-            messages.success(request, 'account was created for ' +  ' ' + user)
-            
-            
-            return redirect('login')
+    if request.user.is_authenticated:
+        return redirect("home")
+    else:
+        form = CreateUserForm()
         
-    context= {
-        'form':form,
-    }
-    return render(request, "accounts/register.html", context )
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('email')
+                messages.success(request, 'account was created for ' +  ' ' + user)
+                
+                
+                return redirect('login')
+            
+        context= {
+            'form':form,
+        }
+        return render(request, "accounts/register.html", context )
 
 
 def login_view(request):
-    if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        
-        user = authenticate(request, email=email, password=password)
-        
-        if user is not None:
-            login(request, user)
-            return redirect('download')
-        else:
-            messages.info(request, 'Username OR password is incorrect')
+    if request.user.is_authenticated:
+        return redirect("home")
+    else:
+        if request.method == 'POST':
+            email = request.POST.get('email')
+            password = request.POST.get('password')
             
-        
-        
-    context= {}
-    return render(request, "accounts/login.html", context )
+            user = authenticate(request, email=email, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('download')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+                
+            
+            
+        context= {}
+        return render(request, "accounts/login.html", context )
 
+@login_required(login_url='login')
 def logout_user(request):
     logout(request)
     return redirect('accounts:login')
--- animate99/apps/main/urls.py
+++ animate99/apps/main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import home
 
 urlpatterns = [
-     path("", home, name='download' )
+     path("", home, name='home' )
 ]
