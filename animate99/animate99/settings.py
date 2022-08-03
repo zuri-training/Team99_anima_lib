ec2ef5b8a8365b09a4d84a4c26c6035e998a360d
gneisscode <103408439+gneisscode@users.noreply.github.com>
2022-08-04 01:10:51 +0200

    static

--- animate99/animate99/settings.py
+++ animate99/animate99/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'staticfiles']
+# STATICFILES_DIRS = [BASE_DIR/'staticfiles']
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
