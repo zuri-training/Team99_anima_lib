1056da66dbfa1f5c77ed2f231c861a3bd2e09433
gneisscode <103408439+gneisscode@users.noreply.github.com>
2022-08-04 09:09:23 +0200

    "edited settings"

--- animate99/animate99/settings.py
+++ animate99/animate99/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'staticfiles']
+# STATICFILES_DIRS = [BASE_DIR/'staticfiles']
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
