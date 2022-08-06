e57c98ccb9625a5b3d3c1712949cc118ba1b7ef7
gneisscode <103408439+gneisscode@users.noreply.github.com>
2022-08-06 10:58:27 +0200

    changed settings

--- animate99/animate99/settings.py
+++ animate99/animate99/settings.py
@@ -145,7 +145,7 @@ STATIC_URL = '/static/'
 # STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 WHITENOISE_USE_FINDERS = True
 # Default primary key field type
