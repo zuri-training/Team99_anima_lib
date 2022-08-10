0823a982c7ec49277e21a188a758f291c9f76d35
gneisscode <103408439+gneisscode@users.noreply.github.com>
2022-08-10 14:05:26 +0200

    added statics and templates

--- animate99/apps/accounts/urls.py
+++ animate99/apps/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from .views import register_view, login_view, logout_user
+from .views import register_view, login_view, logout_user, reset_view, forgot_view
 
 app_name = 'accounts'
 
@@ -7,5 +7,7 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name="login" ),
     path('logout/', logout_user, name="logout" ),
+    path('forgotpassword/', forgot_view, name="forgotpassword" ),
+    path('resetpassword/', reset_view, name="resetpassword" ),
     
 ]
