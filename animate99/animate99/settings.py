91c7cd54e51be579f57fb48aadeca7b9b8b7c856
gneisscode <103408439+gneisscode@users.noreply.github.com>
2022-08-14 06:19:22 +0200

    modified settings.py

--- animate99/animate99/settings.py
+++ animate99/animate99/settings.py
@@ -32,7 +32,7 @@ import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config("DJANGO_DEBUG", default=True, cast=bool)
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'animate-99.herokuapp.com', '.zurifordummies.com', 'localhost', '148.251.6.230',]
 
